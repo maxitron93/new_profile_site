5d321f4166ac0f76d2c332e0ff95ad740c95f825
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-08-13 12:16:48 +1000

    Add page formating

--- resources/app.js
+++ resources/app.js
@@ -1,3 +1,21 @@
+const aboutMePageContent = `
+<div class="page">
+    <p>Test</p>
+</div>`
+
+const placeholderContent = `
+<div class="page">
+    <h1>Placeholder</h1>
+    <h1>Placeholder</h1>
+    <h1>Placeholder</h1>
+    <h1>Placeholder</h1>
+    <h1>Placeholder</h1>
+    <h1>Placeholder</h1>
+    <h1>Placeholder</h1>
+    
+</div>
+`
+
 // -------------------------------------------- //
 // UI Controller
 // -------------------------------------------- //
@@ -23,48 +41,51 @@ const UIController = (() => {
     
             // Generate html
             if (mainNavItem === 'About Me') {
-                subNavHTML = ``
+                subNavHTML = `
+                <div class="row fade-in">
+                    ${aboutMePageContent}
+                </div>`
             } else if (mainNavItem === 'Web Dev') {
                 subNavHTML = `
-                <div class="row fade-in nav-sub-software-dev">
+                <div class="row fade-in">
                     <ul class="tabs">
                         <li class="tab col s2"><a class="active" href="#software-dev-1">Software Dev 1</a></li>
                         <li class="tab col s2"><a href="#software-dev-2">Software Dev 2</a></li>
                         <li class="tab col s2"><a href="#software-dev-3">Software Dev 3</a></li>
                         <li class="tab col s2"><a href="#software-dev-4">Software Dev 4</a></li>
                     </ul>
-                    <div id="software-dev-1" class="col s12">Software Dev 1</div>
-                    <div id="software-dev-2" class="col s12">Software Dev 2</div>
-                    <div id="software-dev-3" class="col s12">Software Dev 3</div>
-                    <div id="software-dev-4" class="col s12">Software Dev 4</div>
+                    <div id="software-dev-1" class="col s12">${placeholderContent}</div>
+                    <div id="software-dev-2" class="col s12">${placeholderContent}</div>
+                    <div id="software-dev-3" class="col s12">${placeholderContent}</div>
+                    <div id="software-dev-4" class="col s12">${placeholderContent}</div>
                 </div>`
             } else if (mainNavItem === 'Android Dev') {
                 subNavHTML = `
-                <div class="row fade-in nav-sub-android-dev">
+                <div class="row fade-in">
                     <ul class="tabs">
                         <li class="tab col s2"><a class="active" href="#android-dev-1">Android Dev 1</a></li>
                         <li class="tab col s2"><a href="#android-dev-2">Android Dev 2</a></li>
                         <li class="tab col s2"><a href="#android-dev-3">Android Dev 3</a></li>
                         <li class="tab col s2"><a href="#android-dev-4">Android Dev 4</a></li>
                     </ul>
-                    <div id="android-dev-1" class="col s12">Android Dev 1</div>
-                    <div id="android-dev-2" class="col s12">Android Dev 2</div>
-                    <div id="android-dev-3" class="col s12">Android Dev 3</div>
-                    <div id="android-dev-4" class="col s12">Android Dev 4</div>
+                    <div id="android-dev-1" class="col s12">${placeholderContent}</div>
+                    <div id="android-dev-2" class="col s12">${placeholderContent}</div>
+                    <div id="android-dev-3" class="col s12">${placeholderContent}</div>
+                    <div id="android-dev-4" class="col s12">${placeholderContent}</div>
                 </div>`
             } else if (mainNavItem === 'Data Science') {
                 subNavHTML = `
-                <div class="row fade-in nav-sub-data-science">
+                <div class="row fade-in">
                     <ul class="tabs">
                         <li class="tab col s2"><a class="active" href="#data-science-1">Data Science 1</a></li>
                         <li class="tab col s2"><a href="#data-science-2">Data Science 2</a></li>
                         <li class="tab col s2"><a href="#data-science-3">Data Science 3</a></li>
                         <li class="tab col s2"><a href="#data-science-4">Data Science 4</a></li>
                     </ul>
-                    <div id="data-science-1" class="col s12">Data Science 1</div>
-                    <div id="data-science-2" class="col s12">Data Science 2</div>
-                    <div id="data-science-3" class="col s12">Data Science 3</div>
-                    <div id="data-science-4" class="col s12">Data Science 4</div>
+                    <div id="data-science-1" class="col s12">${placeholderContent}</div>
+                    <div id="data-science-2" class="col s12">${placeholderContent}</div>
+                    <div id="data-science-3" class="col s12">${placeholderContent}</div>
+                    <div id="data-science-4" class="col s12">${placeholderContent}</div>
                 </div>`
             }
     
@@ -72,7 +93,7 @@ const UIController = (() => {
             DOMstrings.subNav.insertAdjacentHTML('afterbegin', subNavHTML)
     
             // Initialise sub-nav
-            let instance = M.Tabs.init(document.querySelector('.tabs'));
+            let instance = M.Tabs.init(document.querySelector('.tabs'), {swipeable: true});
         }, 500)
     }
 
