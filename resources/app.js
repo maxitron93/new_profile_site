8444f4d814901aa041c6eaa4f97bfa63dbf5c357
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-08-13 15:10:36 +1000

    Add formatting to mobile nav

--- resources/app.js
+++ resources/app.js
@@ -1,23 +1,13 @@
 const aboutMePageContent = `
 <div class="about-page-content">
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
+    <h1>About Me Placeholder</h1>
+    <h1>About Me Placeholder</h1>
+    <h1>About Me Placeholder</h1>
 </div>
 `
 
 const placeholderContent = `
 <div class="page">
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
-    <h1>Placeholder</h1>
     <h1>Placeholder</h1>
     <h1>Placeholder</h1>
     <h1>Placeholder</h1>
@@ -30,10 +20,11 @@ const placeholderContent = `
 const UIController = (() => {
     const DOMstrings = {
         contentContainer: document.querySelector('.content-container'),
-        navMainItem: document.querySelectorAll('.btn-main-nav')
+        navMainItem: document.querySelectorAll('.btn-main-nav'),
+        navMobileItem: document.querySelectorAll('.btn-mobile-nav')
     }
 
-    const loadContentContainer = (mainNavItem) => {
+    const loadContentContainer = (activeItem) => {
         // Define variables
         let subNavHTML
 
@@ -49,12 +40,12 @@ const UIController = (() => {
             }
             
             // Add new content
-            if (mainNavItem === 'About Me') {
+            if (activeItem === 'About Me') {
                 subNavHTML = `
                 <div class="row fade-in about-page-container">
                     ${aboutMePageContent}
                 </div>`
-            } else if (mainNavItem === 'Web Dev') {
+            } else if (activeItem === 'Web Dev') {
                 subNavHTML = `
                 <div class="row fade-in">
                     <ul class="tabs">
@@ -68,7 +59,7 @@ const UIController = (() => {
                     <div id="software-dev-3" class="col s12">${placeholderContent}</div>
                     <div id="software-dev-4" class="col s12">${placeholderContent}</div>
                 </div>`
-            } else if (mainNavItem === 'Android Dev') {
+            } else if (activeItem === 'Android Dev') {
                 subNavHTML = `
                 <div class="row fade-in">
                     <ul class="tabs">
@@ -82,7 +73,7 @@ const UIController = (() => {
                     <div id="android-dev-3" class="col s12">${placeholderContent}</div>
                     <div id="android-dev-4" class="col s12">${placeholderContent}</div>
                 </div>`
-            } else if (mainNavItem === 'Data Science') {
+            } else if (activeItem === 'Data Science') {
                 subNavHTML = `
                 <div class="row fade-in">
                     <ul class="tabs">
@@ -107,21 +98,44 @@ const UIController = (() => {
     }
 
     const changeActiveItem = (target) => {
-        // Remove active from previos main item
+        // Define variable
+        let newActiveTab
+
+        // Remove active from previos main nav item
         DOMstrings.navMainItem.forEach((item) => {
             if (item.className.includes('active')) {
                 item.classList.toggle('active')
             }
         })
 
-        // Add active class to new main item
-        target.parentNode.classList.toggle('active')
+        // Remove active from previos mobile nav item
+        DOMstrings.navMobileItem.forEach((item) => {
+            if (item.className.includes('active')) {
+                item.classList.toggle('active')
+            }
+        })
+
+        // Get new active item
+        newActiveTab = target.innerHTML
+        
+        // Add active to new main nav item
+        DOMstrings.navMainItem.forEach((item) => {
+            if (item.children[0].innerHTML === newActiveTab) {
+                item.classList.toggle('active')
+            }
+        })
+        // Add active to new mobile nav item
+        DOMstrings.navMobileItem.forEach((item) => {
+            if (item.children[0].innerHTML === newActiveTab) {
+                item.classList.toggle('active')
+            }
+        })
     }
 
     return {
         DOMstrings: DOMstrings,
-        loadContentContainer: (mainNavItem) => {
-            loadContentContainer(mainNavItem)
+        loadContentContainer: (activeItem) => {
+            loadContentContainer(activeItem)
         },
         changeActiveItem: (target) => {
             changeActiveItem(target)
@@ -137,22 +151,43 @@ const appController = ((UIController) => {
     // Event Listeners
     const setEventListeners = () => {
 
-        // Event for main-nav buttons
+        // Event listeners for main-nav buttons
         DOMstrings.navMainItem.forEach((btn) => {
             btn.addEventListener('click', (event) => {
                 // Define variables
-                let mainNavItem
+                let activeItem
 
                 // Get nav item clicked
-                mainNavItem = event.target.innerHTML
+                activeItem = event.target.innerHTML
 
                 // Load content
-                UIController.loadContentContainer(mainNavItem)
+                UIController.loadContentContainer(activeItem)
 
                 // Change active class on main nav
                 UIController.changeActiveItem(event.target)
             })
         })
+
+        // Event listeners for main-nav buttons
+        DOMstrings.navMobileItem.forEach((btn) => {
+            btn.addEventListener('click', (event) => {
+                // Define variables
+                let activeItem
+
+                // Get nav item clicked
+                activeItem = event.target.innerHTML
+
+                // Load content
+                UIController.loadContentContainer(activeItem)
+
+                // Change active class on main nav
+                UIController.changeActiveItem(event.target)
+            })
+        })
+
+        // Init and event listeners for side nav
+        let elems = document.querySelectorAll('.sidenav');
+        let instances = M.Sidenav.init(elems);
     }
 
     return {
